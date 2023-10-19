296e04b294df51394cc23d0a8be67821d1543a55
Dongjae Heo <lindsay.heo@gmail.com>
2023-10-19 17:22:51 +1100

    initial

--- src/index.js
+++ src/index.js
@@ -11,12 +11,26 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// Domain - dev-sr0448a5zpclbb7j.us.auth0.com
+// ID - WiAAE4ryTyzGpsdCqAorLq8B7QwBFoPr
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
